dd32da0f31513838aab96d537e4fa7c09a967ea4
Your Name <you@example.com>
2022-12-23 15:27:01 +0100

    cretae reservation page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import DetailsPage from './components/details/DetailsPage';
 import store from './redux/configureStore';
+import AddCar from './components/addCar/addCar';
+import DeleteCar from './components/deleteCar/DeleteCar';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
+        <Route path="/addCar" element={<AddCar />} />
+        <Route path="/deleteCar" element={<DeleteCar />} />
           <Route path="/cars" element={<DetailsPage />} />
         </Routes>
       </Router>
--- src/components/reservations/MyReservations.js
+++ src/components/reservations/MyReservations.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-tabs */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getMyResevationsFromApi } from '../../redux/reducers/myReservations';
+import styles from './MyReservations.module.css';
 
 const MyReservations = () => {
   const dispatch = useDispatch();
@@ -14,35 +14,53 @@ const MyReservations = () => {
     dispatch(getMyResevationsFromApi());
   }, []);
 
-  const handleClick = ((e) => {
+  const handleClick = (e) => {
     navigate(`/cars/${e.target.id}`);
-  });
+  };
   return (
     <>
-      <ul>
-        {
-				currentReservations.map((item) => (
-  <li key={item.id}>
-    <span>
-      Pick up date:
-      {item.pick_up_date}
-    </span>
-    <span>
-      Pick up city:
-      {item.pick_up_city}
-    </span>
-    <span>
-      Drop off date:
-      {item.drop_off_date}
-    </span>
-    <span>
-      Drop up city:
-      {item.return_city}
-    </span>
-    <button type="button" id={item.car_id} onClick={((e) => { handleClick(e); })}>Car details</button>
-  </li>
-				))
-			}
+      <ul className={styles.reservations}>
+        {currentReservations.length === 0 ? (
+          <div className={styles.noReservations}>
+            <p className="text-center">No cars reserved</p>
+          </div>
+        ) : (
+          <table className={styles.tableSucess}>
+            <thead>
+              <tr>
+                <th scope="col">Reservation Id</th>
+                <th scope="col">Pick up date</th>
+                <th scope="col">Drop off date</th>
+                <th scope="col">Pick up city</th>
+                <th scope="col">Drop off city</th>
+                <th scope="col">Car details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentReservations.map((item, index) => (
+                <tr key={item.id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{item.pick_up_date}</td>
+                  <td>{item.drop_off_date}</td>
+                  <td>{item.pick_up_city}</td>
+                  <td>{item.return_city}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className={styles.reserveBtn}
+                      id={item.car_id}
+                      onClick={(e) => {
+                        handleClick(e);
+                      }}
+                    >
+                      Car details
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </ul>
     </>
   );
