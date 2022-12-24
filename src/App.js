4bf51620b076af6d9d925c131922da06b8440751
Your Name <you@example.com>
2022-12-24 19:04:18 +0100

    add bookings

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import DetailsPage from './components/details/DetailsPage';
 import store from './redux/configureStore';
-import AddCar from './components/addCar/addCar';
 import BookingScreen from './routes/Booking';
 import ReservationScreen from './routes/Reservation';
 
@@ -12,8 +11,7 @@ function App() {
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/addCar" element={<AddCar />} />
-          <Route path="/cars" element={<DetailsPage />} />
+           <Route path="/cars" element={<DetailsPage />} />
           <Route element={<BookingScreen />} path="/booking" />
           <Route element={<ReservationScreen />} path="/reservation" />
         </Routes>
