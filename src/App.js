6eaf72f3527cdc4c7dac397ce97306561db7411d
Your Name <you@example.com>
2022-12-24 18:27:48 +0100

    create routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux';
 import DetailsPage from './components/details/DetailsPage';
 import store from './redux/configureStore';
 import AddCar from './components/addCar/addCar';
-import 
+import BookingScreen from './routes/Booking';
+import ReservationScreen from './routes/Reservation';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/addCar" element={<AddCar />} />
           <Route path="/cars" element={<DetailsPage />} />
+          <Route element={<BookingScreen />} path="/booking" />
+          <Route element={<ReservationScreen />} path="/reservation" />
         </Routes>
       </Router>
 
