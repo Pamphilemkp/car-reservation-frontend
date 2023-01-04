cf6e9339ba7ee0bccd8357cb86c19b1a4ad59871
Mark Otuya <markotuya0@gmail.com>
2023-01-04 13:00:17 +0100

    set add-item list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './protected/MainPage';
+import Freelancers from './components/Freelancers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<><h1>Protected Page, Need to Sign in, splash</h1></>}>
+            <Route exact path="/signin" element={<><h1>Sign in Page</h1></>} />
+            <Route exact path="/signup" element={<><h1>Registration/ SignUp Page</h1></>} />
+          </Route>
+          <Route element={<MainPage />}>
+            <Route exact path="/" element={<Freelancers />} />
+            <Route exact path="/details/:id" element={<><h1>Link to FreeLancer&apos;s Details page</h1></>} />
+            <Route exact path="/reserve" element={<><h1>Link to Reservation page</h1></>} />
+            <Route exact path="/myreservation" element={<><h1>Link to My Reservation page</h1></>} />
+            <Route exact path="/addfreelancer" element={<><h1>Link to Add Freelancer page</h1></>} />
+            <Route exact path="/deletefreelancer" element={<><h1>Link to Delete Freelancer Page</h1></>} />
+          </Route>
+          <Route path="*" element={<h1 className="font-bold text-2xl text-center mt-20">404 Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
