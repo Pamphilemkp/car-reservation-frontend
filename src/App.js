2dbb70a50d394e1111b9b6a6772e2d87e3cecdec
PNdunguMaina <ndunguwamaina@gmail.com>
2023-01-05 10:27:33 +0300

    Create details page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
+import DetailsPage from './pages/details/DetailsPaje';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/details" element={<DetailsPage />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
---
+++ src/pages/details/DetailsPaje.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { FetchCar } from '../../redux/store/store';
+import './DetailsPage.css';
+import color from '../../assets/images/color-wheel.png';
+
+function DetailsPage() {
+  const cars = useSelector((state) => state.CarReducers);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchCar());
+  }, [dispatch]);
+
+  return (
+    <div className="app shadow-lg rounded">
+      {cars.map((car) => (
+        <div className="details" key={car.id}>
+          <div className="img-div">
+            <img className="w-100" src={car.photo} alt={car.name} />
+          </div>
+          <div className="box">
+            <header className="d-flex justify-content-end flex-column align-items-end">
+              <h2>{car.name}</h2>
+              <span>{car.brand}</span>
+            </header>
+            <p className="px-3">{car.description}</p>
+            <div className=" booking-div d-flex justify-content-between align-items-center p-2">
+              <span>Booking fee:</span>
+              <span>
+                $
+                {car.price}
+              </span>
+            </div>
+            <div className="duration-div d-flex justify-content-between align-items-center p-2">
+              <span>Duration:</span>
+              <span>1 month</span>
+            </div>
+            <div className="color-div d-flex justify-content-end py-3">
+              <img src={color} alt="" />
+            </div>
+            <div className="color-div d-flex justify-content-end py-3">
+              <button
+                type="button"
+                className="btn reserve-btn d-flex justify-content-between align-items-center"
+              >
+                <i className="fa fa-cog text-white" aria-hidden="true" />
+                <span className="text-white">Reserve</span>
+                <span className="chevron-div">
+                  <i className="fas fa-chevron-right text-white" />
+                </span>
+              </button>
+            </div>
+          </div>
+          <Link to="/">
+            <div className="back-chevron-div">
+              <i className="fas fa-chevron-left text-white" />
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default DetailsPage;
